29776b793be92977464e060696532576c542c954
superhugebetformiladze <broton07@yandex.ru>
2023-12-10 16:40:16 +0400

    generation data

--- database/main.py
+++ database/main.py
@@ -6,15 +6,11 @@ from config_utils import get_db_config
 
 if __name__ == "__main__":
     db_config = get_db_config()
+    
+    create_database(db_config)
+    drop_all_tables(db_config)
+    create_tables(db_config)
+    insert_services_data(db_config, 10)
 
-    # insert_services_data(db_config, 10)
 
-    # drop_all_tables(db_config)
-    # create_tables(db_config)
 
-    # if not database_exists(db_config, db_config['database']):
-    #     create_database(db_config)
-    #     create_tables(db_config)
-    #     generate_data(db_config)
-    # else:
-    #     print(f"База данных '{db_config['database']}' уже существует.")
