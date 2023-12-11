80cef69bd5b8d8fff741029708c5f87ed2d8907a
superhugebetformiladze <broton07@yandex.ru>
2023-12-11 19:26:36 +0400

    made client pages

--- website/client/logic/service_logic.py
+++ website/client/logic/service_logic.py
@@ -37,3 +37,19 @@ def read_service_by_id(service_id):
     except psycopg2.Error as e:
         print(f"Ошибка при получении данных об услуге: {e}")
         return None
+    
+def read_all_services_except(excluded_service_id):
+    try:
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('''
+                    SELECT service_id, service_name, description, additional_info, price, image
+                    FROM services
+                    WHERE service_id != %s
+                ''', (excluded_service_id,))
+                services = cursor.fetchall()
+
+        return services
+    except psycopg2.Error as e:
+        print(f"Ошибка при получении списка услуг: {e}")
+        return None
--- website/client/routes/client_routes.py
+++ website/client/routes/client_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+import random
 
 import sys
 import os
@@ -6,16 +7,33 @@ current_directory = os.path.dirname(os.path.realpath(__file__))
 root_directory = os.path.abspath(os.path.join(current_directory, '..'))
 sys.path.append(root_directory)
 
-from logic.service_logic import read_services, read_service_by_id
+from logic.service_logic import read_all_services_except, read_services, read_service_by_id
 
 client_bp = Blueprint('client', __name__)
 
 @client_bp.route('/')
 def index():
-    services = read_services()
-    return render_template('client/index.html', services=services)
+    all_services = read_services()
+
+    if len(all_services) >= 3:
+        random_services = random.sample(all_services, 3)
+    else:
+        random_services = all_services
+
+    return render_template('client/index.html', services=random_services)
 
 @client_bp.route('/service/<int:service_id>')
 def service_detail(service_id):
     service = read_service_by_id(service_id)
-    return render_template('service_detail.html', service=service)
+    all_services = read_all_services_except(service_id)
+    if len(all_services) >= 3:
+        random_services = random.sample(all_services, 3)
+    else:
+        random_services = all_services
+    return render_template('client/service_detail.html', service=service, services=random_services)
+
+@client_bp.route('/services')
+def services():
+    all_services = read_services()
+    return render_template('client/services.html', services=all_services)
+
